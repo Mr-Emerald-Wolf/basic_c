c513806c2b7cdc3e2c40a5ddd3b9b976e0432faf
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2021-09-14 00:30:42 +0530

    Renamed Folder

--- Practice Set 7 Arrays/reverse.c
+++ Practice Set 7 Arrays/reverse.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 void reverseArray(int arr1[],int n,int rev1[]);
+void reverseList(int arr1[],int len);
 int main()
 {
     int array1[] = {1,2,3,4,5,6,7,10}; //Initialise Array 
@@ -8,11 +9,12 @@ int main()
     printf("Size of array is %d\n",len);
 
     int reverse1[len]; // Reverse list
-    reverseArray(array1,len,reverse1);
+    //reverseArray(array1,len,reverse1);
+    reverseList(array1,len);
 
     for (size_t i = 0; i < len; i++) // Print reversed array 
     {
-        printf("Value of %d element in array is: %d\n",i+1,reverse1[i]);
+        printf("Value of %d element in array is: %d\n",i+1,array1[i]);
     }
     return 0;
 }
@@ -23,4 +25,15 @@ void reverseArray(int arr1[],int n,int rev1[])
         rev1[i] = arr1[n-i-1];
     }
     
+}
+void reverseList(int arr1[],int len)
+{
+    int n,temp;
+    n = len/2;
+    for (size_t i =0; i < n; i++)
+    {
+        temp = arr1[i];
+        arr1[i] = arr1[len - i - 1];
+        arr1[len - i - 1] = temp;
+    }
 }
